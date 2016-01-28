a5148c5b5818d4484642b110d45ba4bb775eae6f
Clark Zhang <czsoup@umich.edu>
2016-01-28 16:34:09 -0500

    started work on new version

--- README.txt
+++ README.txt
@@ -11,6 +11,21 @@ Type "d" to right stroke (turns you to the left)
 MODIFYING PARAMETERS
 there are parameters at the top of the file
 
+# class PaddleMotion(Plan):
+#     def moveMotors(self, hipMotorPos, shoulderMotorPos):
+# 	   self.app.robot.hipMotor.set_pos(hipMotorPos)
+# 	   self.app.robot.shoulderMotor.set_pos(shoulderMotorPos)
 
+# 	def behavior(self):
+# 		T0 = self.app.now
+# 		self.moveMotors(0,3000)
+# 		while abs(self.app.robot.hipMotor.get_pos()-3000)>200:
+# 			self.moveMotors(0,30 * (self.app.now-T0))
+# 			yield self.forDuration(0.05)
+
+#         self.moveMotors(3000,3000)
+# 		yield self.forDuration(0.1)
+#         self.moveMotors(0,3000)
+# 		yield self.forDuration(0.1)
 
 
--- main.py
+++ main.py
@@ -1,237 +1,118 @@
 from joy import *
+import time
 
 # parameters
 hipMotorString = "Nx0C"
 shoulderMotorString = "Nx0B"
 
-class State:
-	NEUTRAL = 1
-	LEFT_BEGIN_NEUTRAL = 2
-	LEFT_BEGIN = 3
-	LEFT_END = 4
-	LEFT_END_NEUTRAL = 5
-	RIGHT_BEGIN_NEUTRAL = 6
-	RIGHT_BEGIN = 7
-	RIGHT_END = 8
-	RIGHT_END_NEUTRAL = 9
-	INVALID = 10
+class RightStrokePlan(Plan):
+	def __init__(self, app, *arg, **kw):
+		Plan.__init__(self, app, *arg, **kw)
+		
+	def behavior(self):
+		self.app.setSpeed(20)
+
+		# targetPos = self.app.shoulderRightBegin
+		# n = 20
+		# totalTime = 0.5
+		# distance = self.app.shoulderRightEnd - self.app.shoulderRightBegin
+
+		# deltaTime = totalTime / n
+		# deltaDistance = distance / n
+
+		# T0 = self.app.now
+		# print("deltaTime: " + str(deltaTime))
+		# for i in range(n):
+		# 	targetPos += deltaDistance
+		# 	self.app.moveMotors(self.app.hipRight, targetPos)
+		# 	print("Target pos: " + str(targetPos))
+		# 	yield self.forDuration(deltaTime)
+
+		self.app.moveMotors(self.app.hipRight, self.app.shoulderRightEnd)
+		while (abs(self.app.shoulderMotor.get_pos() - self.app.shoulderRightEnd) > self.app.motorAccuracy):
+			yield self.forDuration(0.05)
+		while (abs(self.app.hipMotor.get_pos() - self.app.hipRight) > self.app.motorAccuracy):
+			yield self.forDuration(0.05)
+
+class LeftStrokePlan(Plan):
+	def __init__(self, app, *arg, **kw):
+		Plan.__init__(self, app, *arg, **kw)
+
+	def behavior(self):
+		self.app.setSpeed(20)
+		self.app.moveMotors(self.app.hipLeft, self.app.shoulderLeftEnd)
+
+		while (abs(self.app.shoulderMotor.get_pos() - self.app.shoulderLeftEnd) > self.app.motorAccuracy):
+			yield self.forDuration(0.05)
+		while (abs(self.app.hipMotor.get_pos() - self.app.hipLeft) > self.app.motorAccuracy):
+			yield self.forDuration(0.05)
+
+class RightToLeftTransitionPlan(Plan):
+	def __init__(self, app, *arg, **kw):
+		Plan.__init__(self, app, *arg, **kw)
+
+	def behavior(self):
+		self.app.setSpeed(101)
+
+		self.app.moveMotors(self.app.hipLeft, self.app.shoulderLeftBegin)
+		while (abs(self.app.shoulderMotor.get_pos() - self.app.shoulderLeftBegin) > self.app.motorAccuracy):
+			yield self.forDuration(0.05)
+		while (abs(self.app.hipMotor.get_pos() - self.app.hipLeft) > self.app.motorAccuracy):
+			yield self.forDuration(0.05)
+
+class LeftToRightTransitionPlan(Plan):
+	def __init__(self, app, *arg, **kw):
+		Plan.__init__(self, app, *arg, **kw)
+
+	def behavior(self):
+		pass
+
+class RightResetPlan(Plan):
+	def __init__(self, app, *arg, **kw):
+		Plan.__init__(self, app, *arg, **kw)
+
+	def behavior(self):
+		pass
+
+class LeftResetPlan(Plan):
+	def __init__(self, app, *arg, **kw):
+		Plan.__init__(self, app, *arg, **kw)
+
+	def behavior(self):
+		pass
 
 class KayakApp(JoyApp):
 	def __init__(self, hipMotor, shoulderMotor, *arg, **kw):
 		JoyApp.__init__(self, *arg, **kw)
 		self.hipMotorString = hipMotorString
 		self.shoulderMotorString = shoulderMotorString
-		self.motorAccuracy = 150
 
-		self.hipRight = 4600
-		self.hipLeft = -4600
-		self.shoulderLeftBegin = 205
-		self.shoulderLeftEnd = 2700
-		self.shoulderRightBegin = -205
-		self.shoulderRightEnd = -2700
+		self.motorAccuracy = 150
+		self.hipRight = 4200
+		self.hipLeft = -4200
+		self.shoulderLeftBegin = 1000
+		self.shoulderLeftEnd = -2700
+		self.shoulderRightBegin = -1000
+		self.shoulderRightEnd = 2700
 
 		exec("self.hipMotor = self.robot.at." + hipMotorString)
 		exec("self.shoulderMotor = self.robot.at." + shoulderMotorString)
 
-	## GENERAL FUNCTIONS
 	def moveMotors(self, hipMotorPos, shoulderMotorPos):
 		self.hipMotor.set_pos(hipMotorPos)
 		self.shoulderMotor.set_pos(shoulderMotorPos)
-		while(abs(self.hipMotor.get_pos() - hipMotorPos) > self.motorAccuracy):
-			continue
-		while(abs(self.shoulderMotor.get_pos() - shoulderMotorPos) > self.motorAccuracy):
-			continue
 
-	## STATES
-	def gotoState(self, state):
-		if (state == State.NEUTRAL):
-			self.moveMotors(0, 0)
-		elif (state == State.LEFT_BEGIN_NEUTRAL):
-			self.moveMotors(0, self.shoulderLeftBegin)
-		elif (state == State.LEFT_BEGIN):
-			self.moveMotors(self.hipLeft, self.shoulderLeftBegin)
-		elif (state == State.LEFT_END):
-			self.moveMotors(self.hipLeft, self.shoulderLeftEnd)
-		elif (state == State.LEFT_END_NEUTRAL):
-			self.moveMotors(0, self.shoulderLeftEnd)
-		elif (state == State.RIGHT_BEGIN_NEUTRAL):
-			self.moveMotors(0, self.shoulderRightBegin)
-		elif (state == State.RIGHT_BEGIN):
-			self.moveMotors(self.hipRight, self.shoulderRightBegin)
-		elif (state == State.RIGHT_END):
-			self.moveMotors(self.hipRight, self.shoulderRightEnd)
-		elif (state == State.RIGHT_END_NEUTRAL):
-			self.moveMotors(0, self.shoulderRightEnd)
-
-	def currState(self):
-		hipMotorAngle = self.hipMotor.get_pos()
-		hipDifferences = dict()
-		hipDifferences[abs(hipMotorAngle - self.hipLeft)] = 0
-		hipDifferences[abs(hipMotorAngle - 0)] = 1
-		hipDifferences[abs(hipMotorAngle - self.hipRight)] = 2
-
-		shoulderMotorAngle = self.shoulderMotor.get_pos()
-		shoulderRightBeginDiff = abs(shoulderMotorAngle - self.shoulderRightBegin)
-		shoulderRightEndDiff = abs(shoulderMotorAngle - self.shoulderRightEnd)
-		shoulderLeftBeginDiff = abs(shoulderMotorAngle - self.shoulderLeftBegin)
-		shoulderLeftEndDiff = abs(shoulderMotorAngle - self.shoulderLeftEnd)
-
-		closestHipAngle = hipDifferences[min(hipDifferences)]
-
-		if (closestHipAngle == 0):
-			# hip left
-			if (shoulderLeftBeginDiff < shoulderLeftEndDiff):
-				return State.LEFT_BEGIN
-			else:
-				return State.LEFT_END
-		elif (closestHipAngle == 2):
-			# hip right
-			if (shoulderRightBeginDiff < shoulderRightEndDiff):
-				return State.RIGHT_BEGIN
-			else:
-				return State.RIGHT_END
-		else:
-			# hip center
-			shoulderDifferences = dict()
-			shoulderDifferences[shoulderRightBeginDiff] = 0
-			shoulderDifferences[shoulderRightEndDiff] = 1
-			shoulderDifferences[shoulderLeftBeginDiff] = 2
-			shoulderDifferences[shoulderLeftEndDiff] = 3
-			shoulderDifferences[abs(shoulderMotorAngle)] = 4
-
-			closestShoulderAngle = shoulderDifferences[min(shoulderDifferences)]
-
-			return {
-				0 : State.RIGHT_BEGIN_NEUTRAL,
-				1 : State.RIGHT_END_NEUTRAL,
-				2 : State.LEFT_BEGIN_NEUTRAL,
-				3 : State.LEFT_END_NEUTRAL,
-				4 : State.NEUTRAL
-			}.get(closestShoulderAngle, State.INVALID)
-
-	## PATTERNS
-	def strokeRight(self, currState, initialCall = True):
-		if (not initialCall):
-			self.gotoState(currState)
-
-		if (currState == State.NEUTRAL):
-			return self.strokeRight(State.RIGHT_BEGIN_NEUTRAL, False)
-		elif (currState == State.LEFT_BEGIN_NEUTRAL):
-			return self.strokeRight(State.RIGHT_BEGIN_NEUTRAL, False)
-		elif (currState == State.LEFT_BEGIN):
-			return self.strokeRight(State.LEFT_BEGIN_NEUTRAL, False)
-		elif (currState == State.LEFT_END):
-			return self.strokeRight(State.LEFT_END_NEUTRAL, False)
-		elif (currState == State.LEFT_END_NEUTRAL):
-			# return self.strokeRight(State.RIGHT_BEGIN_NEUTRAL, False)
-			return self.strokeRight(State.RIGHT_BEGIN, False)
-		elif (currState == State.RIGHT_BEGIN_NEUTRAL):
-			return self.strokeRight(State.RIGHT_BEGIN, False)
-		elif (currState == State.RIGHT_BEGIN):
-			return self.strokeRight(State.RIGHT_END, False)
-		elif (currState == State.RIGHT_END):
-			if (initialCall):
-				return self.strokeRight(State.RIGHT_END_NEUTRAL, False)
-			else:
-				return
-		elif (currState == State.RIGHT_END_NEUTRAL):
-			return self.strokeRight(State.RIGHT_BEGIN_NEUTRAL, False)
-
-	def strokeLeft(self, currState, initialCall = True):
-		if (not initialCall):
-			self.gotoState(currState)
-
-		if (currState == State.NEUTRAL):
-			return self.strokeLeft(State.LEFT_BEGIN_NEUTRAL, False)
-		elif (currState == State.RIGHT_BEGIN_NEUTRAL):
-			return self.strokeLeft(State.LEFT_BEGIN_NEUTRAL, False)
-		elif (currState == State.RIGHT_BEGIN):
-			return self.strokeLeft(State.RIGHT_BEGIN_NEUTRAL, False)
-		elif (currState == State.RIGHT_END):
-			return self.strokeLeft(State.RIGHT_END_NEUTRAL, False)
-		elif (currState == State.RIGHT_END_NEUTRAL):
-			# return self.strokeLeft(State.LEFT_BEGIN_NEUTRAL, False)
-			return self.strokeLeft(State.LEFT_BEGIN, False)
-		elif (currState == State.LEFT_BEGIN_NEUTRAL):
-			return self.strokeLeft(State.LEFT_BEGIN, False)
-		elif (currState == State.LEFT_BEGIN):
-			return self.strokeLeft(State.LEFT_END, False)
-		elif (currState == State.LEFT_END):
-			if (initialCall):
-				return self.strokeLeft(State.LEFT_END_NEUTRAL, False)
-			else:
-				return
-		elif (currState == State.LEFT_END_NEUTRAL):
-			return self.strokeLeft(State.LEFT_BEGIN_NEUTRAL, False)
-
-	def strokeRightBack(self, currState, initialCall = True):
-		if (not initialCall):
-			self.gotoState(currState)
-
-		if (currState == State.NEUTRAL):
-			self.strokeRightBack(State.RIGHT_END_NEUTRAL, False)
-		elif (currState == State.RIGHT_BEGIN_NEUTRAL):
-			self.strokeRightBack(State.RIGHT_END_NEUTRAL, False)
-		elif (currState == State.RIGHT_BEGIN):
-			if (initialCall):
-				self.strokeRightBack(State.RIGHT_BEGIN_NEUTRAL, False)
-			else :
-				return
-		elif (currState == State.RIGHT_END):
-			self.strokeRightBack(State.RIGHT_BEGIN, False)
-		elif (currState == State.RIGHT_END_NEUTRAL):
-			self.strokeRightBack(State.RIGHT_END, False)
-		elif (currState == State.LEFT_BEGIN_NEUTRAL):
-			self.strokeRightBack(State.RIGHT_END, False)
-		elif (currState == State.LEFT_BEGIN):
-			self.strokeRightBack(State.RIGHT_END, False)
-		elif (currState == State.LEFT_END):
-			self.strokeRightBack(State.LEFT_END_NEUTRAL, False)
-		elif (currState == State.LEFT_END_NEUTRAL):
-			self.strokeRightBack(State.RIGHT_END_NEUTRAL, False)
-
-	def strokeLeftBack(self, currState, initialCall = True):
-		if (not initialCall):
-			self.gotoState(currState)
-
-		if (currState == State.NEUTRAL):
-			self.strokeLeftBack(State.LEFT_END_NEUTRAL, False)
-		elif (currState == State.LEFT_BEGIN_NEUTRAL):
-			self.strokeLeftBack(State.LEFT_END_NEUTRAL, False)
-		elif (currState == State.LEFT_BEGIN):
-			if (initialCall):
-				self.strokeLeftBack(State.LEFT_BEGIN_NEUTRAL, False)
-			else :
-				return
-		elif (currState == State.LEFT_END):
-			self.strokeLeftBack(State.LEFT_BEGIN, False)
-		elif (currState == State.LEFT_END_NEUTRAL):
-			self.strokeLeftBack(State.LEFT_END, False)
-		elif (currState == State.RIGHT_BEGIN_NEUTRAL):
-			self.strokeLeftBack(State.LEFT_END, False)
-		elif (currState == State.RIGHT_BEGIN):
-			self.strokeLeftBack(State.LEFT_END, False)
-		elif (currState == State.RIGHT_END):
-			self.strokeLeftBack(State.RIGHT_END_NEUTRAL, False)
-		elif (currState == State.RIGHT_END_NEUTRAL):
-			self.strokeLeftBack(State.LEFT_END_NEUTRAL, False)		
-
-	def strokeNeutral(self, currState, initialCall = True):
-		self.gotoState(State.NEUTRAL)
-
-	# Higher Level Patterns
-	def strokeForward(self, currState):
-		if currState in [State.RIGHT_BEGIN, State.LEFT_END, State.LEFT_END_NEUTRAL, State.RIGHT_BEGIN_NEUTRAL]:
-			# start with right stroke
-			self.strokeRight(currState)
-			self.strokeLeft(self.currState())
-		else:
-			# start with left stroke
-			self.strokeLeft(currState)
-			self.strokeRight(self.currState())
+	def setSpeed(self, speed):
+		self.hipMotor.set_speed(speed)
+		self.shoulderMotor.set_speed(speed)
 
 	def onStart(self):
-		pass
+		self.rightStrokePlan = RightStrokePlan(self)
+		self.leftStrokePlan = LeftStrokePlan(self)
+		self.rightToLeftTransitionPlan = RightToLeftTransitionPlan(self)
+		self.leftToRightTransitionPlan = LeftToRightTransitionPlan(self)
+		self.rightResetPlan = RightResetPlan(self)
+		self.leftResetPlan = LeftResetPlan(self)
 
 	def onEvent(self, evt):
 		if evt.type == TIMEREVENT:
@@ -240,42 +121,71 @@ class KayakApp(JoyApp):
 		if evt.type != KEYDOWN:
 			return
 
-		if evt.key == K_x:
-			print "state: " + str(self.currState())
-			print "hip: " + str(self.hipMotor.get_pos())
-			print "shoulder: " + str(self.shoulderMotor.get_pos())
-		elif evt.key == K_w:
-			print "Start Forward Stroke"
-			self.strokeForward(self.currState())
-			print "End Forward Stroke"
-		elif evt.key == K_d:
-			print "Start right stroke"
-			self.strokeRight(self.currState())
-			print "End right stroke"
-		elif evt.key == K_a:
-			print "Start left stroke"
-			self.strokeLeft(self.currState())
-			print "End left stroke"
-		elif evt.key == K_s:
-			print "Start neutral position"
-			self.strokeNeutral(self.currState())
-			print "End neutral position"
-		elif evt.key == K_c:
-			print "Start Right Back Stroke"
-			self.strokeRightBack(self.currState())
-			print "End Right Back Stroke"
-		elif evt.key == K_z:
-			print "Start Left Back Stroke"
-			self.strokeLeftBack(self.currState())
-			print "End Left Back Stroke"
-		elif evt.key == K_q:
+		if evt.key == K_q:
 			self.hipMotor.go_slack()
 			self.shoulderMotor.go_slack()
-			print "Slack"
+			print("Slack")
+		elif evt.key == K_a:
+			print("Begin Left")
+			# self.moveMotors(self.hipLeft, self.shoulderLeftBegin)
+			self.leftStrokePlan.start()
+			print("End Left")
+		elif evt.key == K_d:
+			print("Begin Right")
+			# self.moveMotors(self.hipRight, self.shoulderRightBegin)
+			self.rightStrokePlan.start()
+			print("End Right")
+		elif evt.key == K_x:
+			print "hip: " + str(self.hipMotor.get_pos())
+			print "shoulder: " + str(self.shoulderMotor.get_pos())
+			self.rightToLeftTransitionPlan.start()
+
+		# if evt.key == K_q:
+		# 	self.hipMotor.go_slack()
+		# 	self.shoulderMotor.go_slack()
+		# 	print("Slack")
+		# elif evt.key == K_d:
+		# 	print("Begin Right")
+		# 	self.moveMotors(self.hipRight, self.shoulderRightBegin)
+		# 	yield self.forDuration(0.5)
+		# 	self.rightStrokePlan.start()
+		# 	print("End Right")
+
+		# if evt.key == K_x:
+		# 	print "state: " + str(self.currState())
+		# 	print "hip: " + str(self.hipMotor.get_pos())
+		# 	print "shoulder: " + str(self.shoulderMotor.get_pos())
+		# elif evt.key == K_w:
+		# 	print "Start Forward Stroke"
+		# 	self.strokeForward(self.currState())
+		# 	print "End Forward Stroke"
+		# elif evt.key == K_d:
+		# 	print "Start right stroke"
+		# 	self.strokeRight(self.currState())
+		# 	print "End right stroke"
+		# elif evt.key == K_a:
+		# 	print "Start left stroke"
+		# 	self.strokeLeft(self.currState())
+		# 	print "End left stroke"
+		# elif evt.key == K_s:
+		# 	print "Start neutral position"
+		# 	self.strokeNeutral(self.currState())
+		# 	print "End neutral position"
+		# elif evt.key == K_c:
+		# 	print "Start Right Back Stroke"
+		# 	self.strokeRightBack(self.currState())
+		# 	print "End Right Back Stroke"
+		# elif evt.key == K_z:
+		# 	print "Start Left Back Stroke"
+		# 	self.strokeLeftBack(self.currState())
+		# 	print "End Left Back Stroke"
+		# elif evt.key == K_q:
+		# 	self.hipMotor.go_slack()
+		# 	self.shoulderMotor.go_slack()
+		# 	print "Slack"
 
 robot = {"count": 2}
 scr = {}
 
 app = KayakApp(hipMotorString, shoulderMotorString, robot=robot, scr=scr)
 app.run()
-
